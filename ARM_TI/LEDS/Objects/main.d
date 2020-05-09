6940a42991fcbae3fc22cf60a38db77dcf8f75b8
mohamedsaied8 <infint_hero@hotmail.com>
2020-05-09 11:43:07 +0200

    TivaC LEDS

---
+++ ARM_TI/LEDS/Objects/main.d
@@ -0,0 +1,8 @@
+.\objects\main.o: main.c
+.\objects\main.o: C:\Keil_v5\ARM\INC\TI\TM4C123\tm4c123ge6pm.h
+.\objects\main.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm4.h
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmInstr.h
+.\objects\main.o: C:\Keil_v5\ARM\CMSIS\Include\core_cmFunc.h
+.\objects\main.o: C:\Keil_v5\ARM\CMSIS\Include\core_cm4_simd.h
+.\objects\main.o: C:\Keil_v5\ARM\INC\TI\TM4C123\system_TM4C123.h
